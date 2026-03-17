d06a305ab89ede2e0fd443ad185c2ecee9eba104
usmanmahi773-gif <usmanmahi773@gmail.com>
2026-03-18 00:03:26 +0500

    reviewed

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(cors({
     'http://192.168.18.118:5173',
     'https://wolverine-house.netlify.app',
     'http://wolverine-house.netlify.app',
-    'https://wolverineehouse.vercel.app'
+    'https://wolverineehouse-j8o4.vercel.app'
 
   ],
   credentials: true,
